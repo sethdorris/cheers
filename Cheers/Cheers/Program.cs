f279389c0fa7f12f1e4d33a2e3f9baee2e8aa46e
sethdorris <sethc.dorris@gmail.com>
2015-10-15 09:38:37 -0500

    Finished birthday calculations

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Cheers
 {
@@ -16,13 +17,15 @@ namespace Cheers
 
             foreach (char letter in name)
             {
-                if (System.Char.ToUpper(letter).Equals('A') || System.Char.ToUpper(letter).Equals('E') || System.Char.ToUpper(letter).Equals('I') || System.Char.ToUpper(letter).Equals('O') || System.Char.ToUpper(letter).Equals('U'))
+                string vowels = "halfnorsemix";
+
+                if (vowels.IndexOf(letter) == -1) 
                 {
-                    System.Console.WriteLine("Give me an " + letter);
+                    System.Console.WriteLine("Give me a " + letter);
                 }
                 else
                 {
-                    System.Console.WriteLine("Give me a " + letter);
+                    System.Console.WriteLine("Give me an " + letter);
                 }
             }
 
@@ -30,6 +33,23 @@ namespace Cheers
             
 
             System.Console.WriteLine(capLetter + name.Substring(1) + " is Awesome!");
+            System.Console.WriteLine("Hey " + capLetter + name.Substring(1) + " What's your birthday? Enter as mm/dd");
+            string userInput = System.Console.ReadLine();
+            DateTime birthday = Convert.ToDateTime(userInput);
+            DateTime now = DateTime.Now.Date;
+            //System.Console.WriteLine("Your birthday is on " + birthday);
+            //System.Console.WriteLine("Today's date is " + now);
+            TimeSpan daysTo = birthday - now;
+            int days = daysTo.Days;
+
+            if (days < 0)
+            {
+                days = days + 365;
+            }
+           
+            System.Console.WriteLine("It is " + days + " days until your birthday");
+
+    
 
             System.Console.WriteLine("Press any key to exit");
             System.Console.ReadLine();
