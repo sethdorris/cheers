9e6e4b0df8eff7ee7440c06c4ea614444072bd2c
sethdorris <sethc.dorris@gmail.com>
2015-10-15 14:07:13 -0500

    Split main into a main and birthday cheer method

--- Cheers/Cheers/Program.cs
+++ Cheers/Cheers/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Globalization;
 
 namespace Cheers
 {
@@ -15,31 +14,36 @@ namespace Cheers
             string name = System.Console.ReadLine();
             System.Console.WriteLine("Hi, " + name);
 
-            foreach (char letter in name)
+            foreach (char letter in name.ToLower())
             {
                 string vowels = "halfnorsemix";
 
-                if (vowels.IndexOf(letter) == -1) 
+                if (Char.IsLetter(letter))
                 {
-                    System.Console.WriteLine("Give me a " + letter);
-                }
-                else
-                {
-                    System.Console.WriteLine("Give me an " + letter);
+                    if (vowels.Contains(letter) == false)
+                    {
+                        System.Console.WriteLine("Give me a " + letter);
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Give me an " + letter);
+                    }
                 }
             }
-
             char capLetter = System.Char.ToUpper(name[0]);
-            
-
-            System.Console.WriteLine(capLetter + name.Substring(1) + " is Awesome!");
+            System.Console.WriteLine(capLetter + name.Substring(1).ToUpper() + " is Awesome!");
             string formattedName = capLetter + name.Substring(1);
             System.Console.WriteLine("Hey " + formattedName + " What's your birthday? Enter as mm/dd");
+            BirthdayCheer(formattedName);
+            System.Console.WriteLine("Press any key to exit");
+            System.Console.ReadLine();
+        }
+
+        private static void BirthdayCheer(string formattedName)
+        {
             string userInput = System.Console.ReadLine();
             DateTime birthday = Convert.ToDateTime(userInput);
             DateTime now = DateTime.Now.Date;
-            //System.Console.WriteLine("Your birthday is on " + birthday);
-            //System.Console.WriteLine("Today's date is " + now);
             TimeSpan daysTo = birthday - now;
             int days = daysTo.Days;
 
@@ -47,7 +51,9 @@ namespace Cheers
             {
                 days = days + 365;
                 System.Console.WriteLine("It is " + days + " days until your birthday");
-            } else {
+            }
+            else
+            {
                 System.Console.WriteLine("It is " + days + " days until your birthday");
             }
 
@@ -55,13 +61,6 @@ namespace Cheers
             {
                 System.Console.WriteLine("Happy Birthday " + formattedName);
             }
-           
-            
-
-    
-
-            System.Console.WriteLine("Press any key to exit");
-            System.Console.ReadLine();
         }
     }
 }
